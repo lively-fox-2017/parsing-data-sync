7c48c49dcff12e70e2f149b391fe0724068de186
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-25 12:10:36 +0700

    Commit Release 2

--- parser.js
+++ parser.js
@@ -10,7 +10,7 @@ class Person {
     this.last_name = last_name;
     this.email = email;
     this.phone = phone;
-    this.created_at = created_at;
+    this.created_at = new Date(created_at);
   }
 }
 
@@ -19,27 +19,36 @@ class PersonParser {
   constructor(file) {
     this._file = file;
     this._people = [];
-    let fileParsed = fs.readFileSync(file).toString().split('\r\n');
+  }
+
+  parseFile() {
+    let fileParsed = fs.readFileSync(this._file).toString().split('\r\n');
     fileParsed.forEach((value, key) => {
       if (key !== 0) {
-        this._people.push(value);
+        let person = new Person(...value.split(','));
+        this._people.push(person);
       }
     })
   }
 
   get people() {
-    return this._people
+    return this._people;
   }
 
   addPerson(person) {
-    let str = '\n' + person.id + ',' + person.first_name + ',' + person.last_name + ',' + person.email + ',' + person.phone + ',' + person.created_at;
+    person.id = parseInt(this._people[this._people.length - 1].id) + 1;
+    let str = person.id + ',' + person.first_name + ',' + person.last_name + ',' + person.email + ',' + person.phone + ',' + person.created_at.toISOString();
+    this._people.push(str);
+    str = '\r\n' + str;
     fs.appendFileSync(this._file, str);
   }
-
 }
 
 let parser = new PersonParser('./people.csv')
-// console.log(parser.people);
+parser.parseFile();
+console.log(require('util').inspect(parser.people, {
+  maxArrayLength: null
+}));
+console.log(`There are ${parser.people.length} people in the file '${parser._file}'.`)
 parser.addPerson(new Person(201, 'Saptanto', 'Sindu', 'Saptanto.sindu@gmail.com', '08999175696', '2012-02-22T10:09:03-08:00'))
-console.log(parser._people[201]);
 console.log(`There are ${parser.people.length} people in the file '${parser._file}'.`)
