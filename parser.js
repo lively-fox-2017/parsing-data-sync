3febe0008503bd4ae56c49762b3eb609d0e48f91
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-25 10:58:11 +0700

    penambahan constructor Person

--- parser.js
+++ parser.js
@@ -1,8 +1,18 @@
 "use strict"
 
+const fs = require('fs')
+
 class Person {
   // Look at the above CSV file
   // What attributes should a Person object have?
+  constructor(id, first_name, last_name, email, phone, created_at) {
+    this._id = id;
+    this._first_name = first_name;
+    this._last_name = last_name;
+    this._email = email;
+    this._phone = phone;
+    this._created_at = created_at;
+  }
 }
 
 class PersonParser {
