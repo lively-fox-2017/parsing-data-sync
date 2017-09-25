ad0b2a0e07b5d70339413dee9447b011d0d39b83
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-25 11:26:01 +0700

    Commit Release 1

--- parser.js
+++ parser.js
@@ -19,7 +19,7 @@ class PersonParser {
   constructor(file) {
     this._file = file;
     this._people = [];
-    let fileParsed = fs.readFileSync(file).toString().split('\n');
+    let fileParsed = fs.readFileSync(file).toString().split('\r\n');
     fileParsed.forEach((value, key) => {
       if (key !== 0) {
         this._people.push(value);
@@ -31,9 +31,15 @@ class PersonParser {
     return this._people
   }
 
-  addPerson() {}
+  addPerson(person) {
+    let str = '\n' + person.id + ',' + person.first_name + ',' + person.last_name + ',' + person.email + ',' + person.phone + ',' + person.created_at;
+    fs.appendFileSync(this._file, str);
+  }
 
 }
 
 let parser = new PersonParser('./people.csv')
+// console.log(parser.people);
+parser.addPerson(new Person(201, 'Saptanto', 'Sindu', 'Saptanto.sindu@gmail.com', '08999175696', '2012-02-22T10:09:03-08:00'))
+console.log(parser._people[201]);
 console.log(`There are ${parser.people.length} people in the file '${parser._file}'.`)
