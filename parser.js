f003b23d3fe010ce784ce7ef00a09e244e57ab8b
setiadijoe <setiadi_y56@yahoo.co.id>
2017-09-25 11:20:02 +0700

    parsing data synchronus

--- parser.js
+++ parser.js
@@ -19,17 +19,42 @@ class PersonParser {
 
   constructor(file) {
     this._file = file
-    this._people = null
+    this._people = []
+    this._stringpeople = fs.readFileSync(this._file, 'UTF-8')
+    this._arrpeople = this._stringpeople.split('\n')
   }
 
-  get people() {
+  createObj() {
+
+    for (var i = 1; i < this._arrpeople.length; i++) {
+      var namaorang = this._arrpeople[i].split(',');
+      namaorang[5] = new Date(namaorang[5])
+      var orang = new Person(namaorang[0], namaorang[1], namaorang[2], namaorang[3], namaorang[4], namaorang[5])
+      this._people.push(orang);
+    }
     return this._people
   }
 
-  addPerson() {}
+  get people() {
+    this._people = this.createObj()
+    return { size: this._people.length, file: this._file };
+  }
+
+  addPerson(personplus) {
+    var tambah = personplus
+    this.tambah = tambah
+    return this.tambah
+  }
+
+  save() {
+    fs.appendFileSync('./people.csv', this.tambah + '\n');
+  }
 
 }
 
 let parser = new PersonParser('people.csv')
+// console.log(parser.people)
+// console.log(parser.addPerson(['201', 'Prana', 'Kamal', 'kita@google.com', '191-984-89-89', '2012-13T45:08:22-09-2012']));
+// parser.save()
 
-console.log(`There are ${parser.people.size} people in the file '${parser.file}'.`)
+console.log(`There are ${parser.people.size-1} people in the file '${parser.people.file}'.`)
